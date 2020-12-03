68331e95e5c08ad495d9e98eec7a4d486ec32e23
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2020-12-03 14:03:46 +0100

    toevoegen oudere autos

--- script/script.js
+++ script/script.js
@@ -1,32 +1,44 @@
 let car = document.querySelector('.car');
-let section = document.querySelector('main')
+let main = document.querySelector('main')
 let allAnimationItems = document.querySelectorAll('.animate')
-let sectionHeight = section.offsetHeight;
+let mainHeight = main.offsetHeight;
 let windowHeight = window.innerHeight;
-let sectionInnerHeight = sectionHeight - windowHeight;
+let mainInnerHeight = mainHeight - windowHeight;
 
 let allSections = document.querySelectorAll('main > section')
+console.log(main.offsetTop)
 
 window.addEventListener("scroll", (e) => {
-    let scroll = this.scrollY;
-    let percentage = scroll / sectionInnerHeight * 100;
+    let scroll = this.scrollY - main.offsetTop;
+    let percentage = scroll / mainInnerHeight * 100;
     percentage = parseInt(percentage)
-    let toLeft = 100 - percentage
-    car.style.setProperty('--car-left', toLeft + "%")
+    let toLeft = percentage
 
+    if (percentage > 40) {
+        car.style.backgroundImage = "url('./style/images/car-old.png')"
+        car.style.bottom = "25px"
+    }
+    else if (percentage > 20) {
+        car.style.backgroundImage = "url('./style/images/car-older.png')"
+        car.style.bottom = "15px"
+    }
+    else {
+        car.style.backgroundImage = "url('./style/images/car-new.png')"
+        car.style.bottom = "25px"
+    }
+    car.style.setProperty('--car-left', toLeft + "%")
 });
+
 
 // OBSERVER
 const options = {
-    threshold: 1
+    rootMargin: "20px",
+    threshold: 0.2
 }
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
             entry.target.style.animation = `${entry.target.dataset.animation} .8s forwards ease-in-out`
-        }
-        else {
-            entry.target.style.animation = 'none'
         }
     })
 }, options)
